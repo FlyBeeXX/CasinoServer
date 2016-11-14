a27c4836073d9aeae39058c909030ab230d879c6
fp-lijian <Jian.Li@funplus.com>
2016-11-14 14:26:03 +0800

    [Server] updated.

--- src/github.com/name5566/leaf/util/util.go
+++ src/github.com/name5566/leaf/util/util.go
@@ -2,8 +2,27 @@ package util
 
 import (
     "time"
+    "crypto/md5"
+    "encoding/hex"
+    "io"
+    "crypto/rand"
+    "encoding/base64"
 )
 
 func NowUTCTime() (int64){
     return time.Now().UTC().UnixNano() / 1000000;
+}
+
+func GetMd5(s string) string {
+    h := md5.New()
+    h.Write([]byte(s))
+    return hex.EncodeToString(h.Sum(nil))
+}
+
+func GetGuid() string {
+    b := make([]byte, 48)
+    if _, err := io.ReadFull(rand.Reader, b); err != nil {
+        return ""
+    }
+    return GetMd5(base64.URLEncoding.EncodeToString(b))
 }
